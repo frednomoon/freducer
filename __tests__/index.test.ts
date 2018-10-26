da298e47d5571c2771cd848ab2068e7301341cc1
a <fred.shepherd@music-group.com>
2018-10-26 11:25:25 +0100

    improve tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,11 +2,37 @@ import freducer, { asyncReducer, asyncMethod } from '../src'
 
 const GET_RESOURCE = 'GET_RESOURCE'
 
-const store = {
-  pending: false,
-  error: null,
-  success: false,
-  data: null
+const testStores = {
+  initialStore: {
+    success: false,
+    pending: false,
+    error: null,
+    data: null
+  },
+  store: {
+    success: true,
+    pending: false,
+    error: null,
+    data: 'some data'
+  },
+  errorStore: {
+    success: true,
+    pending: false,
+    error: 'you got an error boiii',
+    data: 'previous data'
+  },
+  pendingAfterErrorStore: {
+    success: false,
+    pending: true,
+    error: 'its an error pal',
+    data: 'still got data tho'
+  },
+  pendingAfterSuccessStore: {
+    success: true,
+    pending: true,
+    error: null,
+    data: 'still got data tho'
+  }
 }
 
 const payload = {
@@ -21,55 +47,56 @@ basicImplementation(asyncReducer({
 
 function basicImplementation(reducer, title) {
   describe(`basic implementation for ${title}`, () => {
-    it('_PENDING should set pending state', () => {
-      const action = {
-        type: `${GET_RESOURCE}_PENDING`,
-        payload
-      }
+    Object.keys(testStores).forEach(key => {
+      it(`_PENDING should set pending state - ${key}`, () => {
+        const action = {
+          type: `${GET_RESOURCE}_PENDING`,
+          payload
+        }
 
-      const state = reducer(store, action)
+        const state = reducer(testStores[key], action)
 
-      // pending should be set to true
-      expect(state.pending).toBe(true)
-      // data should remain unchanged
-      expect(state.data).toBe(store.data)
+        // pending should be set to true
+        expect(state.pending).toBe(true)
+        // all other parts should persist from previous state
+        expect(state.data).toBe(testStores[key].data)
+        expect(state.error).toBe(testStores[key].error)
+        expect(state.success).toBe(testStores[key].success)
+      })
+      it(`_REJECTED should set error state - ${key}`, () => {
+        const action = {
+          type: `${GET_RESOURCE}_REJECTED`,
+          payload
+        }
 
-      // trivial
-      expect(state.error).toBe(store.error)
-      expect(state.success).toBe(store.success)
-    })
-    it('_REJECTED should set error state', () => {
-      const action = {
-        type: `${GET_RESOURCE}_REJECTED`,
-        payload
-      }
+        const state = reducer(testStores[key], action)
 
-      const state = reducer(store, action)
+        // error should be set to true
+        expect(state.error).toBe(payload)
+        // should set success to false
+        expect(state.success).toBe(false)
+        // pending should be false
+        expect(state.pending).toBe(false)
+        // data should persist from the the previous state
+        expect(state.data).toEqual(testStores[key].data)
+      })
+      it(`_FULFILLED should set success state - ${key}`, () => {
+        const action = {
+          type: `${GET_RESOURCE}_FULFILLED`,
+          payload
+        }
 
-      // error should be set to true
-      expect(state.error).toBe(payload)
+        const state = reducer(testStores[key], action)
 
-      // trivial
-      expect(state.data).toEqual(store.data)
-      expect(state.pending).toBe(store.pending)
-      expect(state.success).toBe(store.success)
-    })
-    it('_FULFILLED should set success state', () => {
-      const action = {
-        type: `${GET_RESOURCE}_FULFILLED`,
-        payload
-      }
-
-      const state = reducer(store, action)
-
-      // error should be set to true
-      expect(state.success).toBe(true)
-      // data should equal payload
-      expect(state.data).toEqual(payload)
-
-      // trivial
-      expect(state.pending).toBe(store.pending)
-      expect(state.error).toBe(store.error)
+        // error should be set to true
+        expect(state.success).toBe(true)
+        // data should equal payload
+        expect(state.data).toEqual(payload)
+        // should set error to null
+        expect(state.error).toBe(null)
+        // pending should be false
+        expect(state.pending).toBe(false)
+      })
     })
   })
 }
